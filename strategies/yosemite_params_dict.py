3e324942c5331315207046660e84b2796de5afac
Eric Ervin <eervin@realityshares.com>
2022-03-31 05:58:11 -0700

    created new yosemite strategy

--- strategies/yosemite_params_dict.py
+++ strategies/yosemite_params_dict.py
@@ -1,5 +1,6 @@
-{"asset" : "BINANCEETHUSDT",
-    "eth_2h_low_dd":{
+{
+    "asset" : "BINANCEETHUSDT",
+        "eth_2h_low_dd":{
         "start": 0.018, 
         "increment": 0.023, 
         "maximum": 0.3, 
@@ -36,6 +37,19 @@
         "order size": 60, 
         "commission": 0.02, 
         "margin long": 50, 
-        "margin short": 50
+        "margin short": 50,
         },
-}
+    "eth_90m_low_freq":{
+        "start": 0.02, 
+        "increment": 0.023, 
+        "maximum": 0.07, 
+        "initial trade size": 100, 
+        "how many ticks": 100, 
+        "take profit change %": 7, 
+        "daily profit target change": 7, 
+        "order size": 100, 
+        "commission": 0.04, 
+        "margin long": 50, 
+        "margin short": 50,
+        },
+    }
