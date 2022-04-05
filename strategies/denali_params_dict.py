87d310e10e914fc5440ac451ff0b8cee183696b9
Eric Ervin <eervin@realityshares.com>
2022-04-05 04:56:17 -0700

    updated a new strategy for BNB

--- strategies/denali_params_dict.py
+++ strategies/denali_params_dict.py
@@ -1,5 +1,5 @@
 {"asset" : "BINANCEBNBUSDT",
-    "bnb_1h_solid_base":{
+    "bnb_1h_solid_base":{ # Note, this works well at 8 hours for BTC as well
         "Power1": 1.05,
         "Power2": 0.90,
         "Power3": 0.005,
@@ -10,5 +10,16 @@
         "trimShort": 2,
         "trimLong": 2,
         },
+    "bnb_1h_max_opt":{
+        "Power1": 1.485471,
+        "Power2": 0.696677,
+        "Power3": 0.0027197,
+        "fastEMA": 10,
+        "slowEMA": 82,
+        "fastSMA": 83,
+        "slowSMA": 38, # NOTE: This is the slowSMA for the 1h_max_opt however it is actually faster than fast EMA
+        "trimShort": 0.1470315,
+        "trimLong": 0.07660551,
+    },
     
 }
